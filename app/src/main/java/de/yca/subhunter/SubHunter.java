7f1ca59759c87ff5a5e6da24847021765b2309f0
yohann <soleil.genex7@gmail.com>
2023-12-30 13:44:50 +0100

    Init SubHunter screen

--- app/src/main/java/de/yca/subhunter/SubHunter.java
+++ app/src/main/java/de/yca/subhunter/SubHunter.java
@@ -1,14 +1,16 @@
 package de.yca.subhunter;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+//import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.view.Window;
 import android.os.Bundle;
 
-public class SubHunter extends AppCompatActivity {
+public class SubHunter extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
     }
 }
